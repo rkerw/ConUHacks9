d5cd8596b1a53293eb95ba202db2c1e6fdab439f
rkerw <rowan.kerwin@mail.mcgill.ca>
2025-02-01 15:56:14 -0500

    fishnet setup

--- ThisTown/Assets/Scripts/PlayerController.cs
+++ ThisTown/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FishNet.Connection;
+using FishNet.Object;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
 
     public float speed;
     public Rigidbody2D rb;
     private Vector2 moveInput;
 
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        if (base.IsOwner)
+        {
+            // nothing atm
+        }
+        else
+        {
+            gameObject.GetComponent<PlayerController>().enabled = false;
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
+
 
     // Update is called once per frame
     void Update()
