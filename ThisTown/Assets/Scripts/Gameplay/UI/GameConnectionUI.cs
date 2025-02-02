b3a59c9c764f2d7e42f29e7b6b4c34ea1c96605f
Sadiqali Shaik <sadiq2001ali@gmail.com>
2025-02-02 18:26:02 +0530

    menu and best bamboozle ever

--- ThisTown/Assets/Scripts/Gameplay/UI/GameConnectionUI.cs
+++ ThisTown/Assets/Scripts/Gameplay/UI/GameConnectionUI.cs
@@ -7,17 +7,23 @@ public class GameConnectionUI : MonoBehaviour
     [SerializeField] private Button serverStart;
     [SerializeField] private Button clientStart;
 
+    [SerializeField] private GameObject serverWaitMsg;
+    [SerializeField] private GameObject clientWaitMsg;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         clientStart.onClick.AddListener(() =>
         {
             GameStateMananger.Instance.StartGameAsClient(ipAddrField.text);
+            clientWaitMsg.gameObject.SetActive(true);
         });
 
         serverStart.onClick.AddListener(() =>
         {
             GameStateMananger.Instance.StartGameAsServer(ipAddrField.text);
+            serverWaitMsg.gameObject.SetActive(true);
+            
         });
     }
 }
