6672fb22c379fe83cb7848927dfb5bd08587564b
rkerw <rowan.kerwin@mail.mcgill.ca>
2025-02-02 03:21:16 -0500

    basic player animation

--- ThisTown/Assets/Scripts/PlayerMovementController.cs
+++ ThisTown/Assets/Scripts/PlayerMovementController.cs
@@ -10,6 +10,14 @@ public class PlayerMovementController : NetworkBehaviour
     [SerializeField]
     private Vector3 moveInput;
     private Rigidbody2D rb2d;
+    private Animator animator;
+    private bool isFacingRight = true;
+
+    public void flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
 
 
     public override void OnStartClient()
@@ -20,6 +28,10 @@ public class PlayerMovementController : NetworkBehaviour
     public void UpdateMovement()
     {
        moveInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+       if (moveInput.magnitude > 1)
+       {
+           moveInput.Normalize();
+       }
     }
 
     private void FixedUpdate()
@@ -30,8 +42,29 @@ public class PlayerMovementController : NetworkBehaviour
             rb2d.bodyType = RigidbodyType2D.Kinematic;
         }
 
+        if (animator == null)
+        {
+            animator = GetComponentInChildren<Animator>();
+        }
+
         var newPosition = transform.position + (moveInput * moveSpeed * Time.fixedDeltaTime);
         rb2d.MovePosition(newPosition);
+
+        if (moveInput.x != 0 || moveInput.y != 0)
+        {
+            animator.SetBool("isMoving", true);
+        } else {
+            animator.SetBool("isMoving", false);
+        }
+
+        if (moveInput.x > 0 && !isFacingRight)
+        {
+            flip();
+        }
+        else if (moveInput.x < 0 && isFacingRight)
+        {
+            flip();
+        }
         
     }
 }
